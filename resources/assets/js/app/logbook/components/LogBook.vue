bab027a5cb86b8f733c037877ba78496e3c20a31
Markavelii <gergs.forex@gmail.com>
2017-10-17 23:37:57 +1100

    add sets

--- resources/assets/js/app/logbook/components/LogBook.vue
+++ resources/assets/js/app/logbook/components/LogBook.vue
@@ -1,20 +1,46 @@
 <template>
-	<div>
-		<p>{{ index }}</p>
-	</div>
+	
+	<div class="">
+        <input type="text" class="add__exercise" placeholder="Add set" v-on:keyup="addSet" v-model="newSet" autofocus>
+           
+        <!-- <p>{{ index }} 👀</p> -->
+        <!-- <div class="set__list">
+        	<p>{{ set }}</p>
+        </div> -->
+        <div class="sets-section">
+        	<div class="sets-border">
+        		<ul class="sets">
+		        	<li v-for="set in sets" class="exercise">
+        				{{ set }}
+        			</li>
+        		</ul>
+        	</div>
+        </div>
+    </div>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				index: null
+				index: null,				
+				newSet: null,
+				sets: [],
 			}
 		},
 		mounted() {
 			axios.get('/api/logbook').then((response) => {
-				this.index = response.data.data
+				this.index = response.data.data;
 			})
+		},
+		methods: {
+			addSet(e) {
+				if (e.keyCode === 13) {
+					console.log(e);
+					this.sets.push(this.newSet);
+					this.newSet = "";
+				}
+			}
 		}
 	}
 
